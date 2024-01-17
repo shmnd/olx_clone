3468feb960632dc645ffd1f88880843d6d351415
“shamnad” <hamzashamnad123@gmail.com>
2024-01-17 16:03:30 +0530

    olx first push

--- src/Components/Signup/Signup.js
+++ src/Components/Signup/Signup.js
@@ -1,49 +1,69 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Logo from '../../olx-logo.png';
 import './Signup.css';
 
 export default function Signup() {
+
+  const [username,setUsername]=useState('')
+  const [email,setEmail]=useState('')
+  const [phone,setPhone]=useState('')
+  const [pass,setPass]=useState('')
+
+
+
   return (
     <div>
       <div className="signupParentDiv">
         <img width="200px" height="200px" src={Logo}></img>
         <form>
+
           <label htmlFor="fname">Username</label>
           <br />
           <input
             className="input"
             type="text"
+            value={username}
+            onChange={(e)=>setUsername(e.target.value)}
             id="fname"
             name="name"
-            defaultValue="John"
+            defaultValue=""
           />
           <br />
+
           <label htmlFor="fname">Email</label>
           <br />
           <input
             className="input"
             type="email"
+            value={email}
+            onChange={(e)=>setEmail(e.target.value)}
             id="fname"
             name="email"
-            defaultValue="John"
+            defaultValue="@gmail.com"
           />
           <br />
+
           <label htmlFor="lname">Phone</label>
           <br />
           <input
             className="input"
             type="number"
+            value={phone}
+            onChange={(e)=>setPhone(e.target.value)}
             id="lname"
             name="phone"
             defaultValue="Doe"
           />
           <br />
+
           <label htmlFor="lname">Password</label>
           <br />
           <input
             className="input"
             type="password"
+            value={pass}
+            onChange={(e)=>setPass(e.target.value)}
             id="lname"
             name="password"
             defaultValue="Doe"
